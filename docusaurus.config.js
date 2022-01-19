ba6c56f1a966a01b958f4640a682f41704148614
Chase 李 <chase@pawbyte.com>
2022-01-19 10:14:23 -0600

    Update edit URL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,7 +24,7 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
-          editUrl: "https://github.com/pawbyte/gpe-docs/",
+          editUrl: "https://github.com/pawbyte/gpe-docs/edit/main/",
         },
         blog: {
           showReadingTime: true,
